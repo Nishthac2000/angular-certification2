637d9ea42d201e8e6d022deee437232438669b72
Nishthac2000 <112468493+Nishthac2000@users.noreply.github.com>
2023-02-25 16:30:39 +0530

    Initial commit

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { RapidApiService } from '../../services/rapid-api.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Team } from '../../models/team';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+})
+export class HomePageComponent implements OnInit {
+  constructor(private rapidApi: RapidApiService) {}
+
+  teams: Team[] = [];
+  teamsList: Team[] = [];
+
+  // teamId!: number;
+
+  form = new FormGroup({
+    teamName: new FormControl('1'),
+  });
+
+  ngOnInit(): void {
+    this.loadTeams();
+  }
+
+  loadTeams() {
+    this.rapidApi.getTeamData().subscribe((res: any) => {
+      this.teams = res.data;
+    });
+  }
+}
